e8e7d89b9878a1e3d05b2e04afb75f1e9214f73e
DarkSharpness <2040703891@qq.com>
2023-09-26 14:53:02 +0800

    Fix: Fix command line parsing.

--- src/main.cpp
+++ src/main.cpp
@@ -16,22 +16,39 @@
 #include "ASMbuilder.h"
 #include "ASMallocator.h"
 
-bool type = false;
+/**
+ * Bit 0: -S / -s
+ * Bit 1: -fsyntax-only
+ * Bit 2: -ll
+*/
+std::bitset <3> BitMap = 0;
 
 int main(int argc, const char* argv<::>) <%
-    if(argc > 1) {
-        std::string_view __v = argv[1];
-        --argc , ++argv;
-        if (__v == "-ll") { type = true; }
-        else if (__v != "-s" || __v != "-S") {
-            std::cerr << "Unknown option: " << __v << std::endl;
-            return 1;
+    int __len = std::min(argc,2);
+    for(int i = 0 ; i < __len ; ++i) {
+        const char *__ptr = argv[i];
+        if(std::strcmp("-S",__ptr) == 0) {
+            BitMap.set(0);
+            argc = 0; /* OJ-mode */
+            break;
+        } else if (std::strcmp("-s",__ptr) == 0) {
+            ++argv, --argc;
+            BitMap.set(0);
+        } else if(std::strcmp("-fsyntax-only",__ptr) == 0) {
+            BitMap.set(1);
+            argc = 0; /* OJ-mode */
+            break;
+        } else if(std::strcmp("-ll",__ptr) == 0) {
+            ++argv, --argc;
+            BitMap.set(2);
+            break;
         }
     }
+    /* Invalid : OJ-mode and -s */
+    if (BitMap.count() != 1) BitMap = 1,argc = 0;
 
     try {
         dark::OPT::optimize_options::init(argc,argv);
-        // freopen("test.in","r",stdin);
         MxErrorListener listener;
 
         antlr4::ANTLRInputStream input(std::cin);
@@ -51,16 +68,22 @@ int main(int argc, const char* argv<::>) <%
         dark::AST::ASTvisitor Conless {Wankupi.global,Wankupi.mapping};
         dark::IR::IRbuilder Hastin {Conless.global,Conless.class_map,Wankupi.global};
 
+        if (BitMap[1]) return 0;
+
         if (dark::OPT::optimize_options::get_state().is_enabled() > 0)
             dark::OPT::SSAbuilder {Hastin.global_variables,Hastin.global_functions};
-        if (type) { Hastin.debug_print(std::cout); return 0; }
+
+        if (BitMap[2]) {
+            Hastin.debug_print(std::cout);
+            return 0;
+        }
 
         dark::ASM::ASMbuilder YYU {Hastin.global_variables,Hastin.global_functions};
 
         for(auto __func : YYU.global_info.function_list)
             dark::ASM::ASMallocator {__func};
 
-        if (!type) YYU.global_info.print(std::cout);
+        if (BitMap[0]) YYU.global_info.print(std::cout);
     } catch(dark::error &err) {
         return 1;
     } catch(std::exception &err) {
